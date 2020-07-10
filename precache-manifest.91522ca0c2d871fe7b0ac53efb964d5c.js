bd4684a2f9438f6b45817d14b85825ac2516b1d1
Vitor Ivan D'Angelo <metroid.p2p@gmail.com>
2020-07-10 16:27:03 -0300

    Updates

---
+++ precache-manifest.91522ca0c2d871fe7b0ac53efb964d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a483ff97336ccd380bec1a0de74bce8",
+    "url": "/framer-motion-react/index.html"
+  },
+  {
+    "revision": "9d5d92100c80b7c659b4",
+    "url": "/framer-motion-react/static/css/main.7ad2ec85.chunk.css"
+  },
+  {
+    "revision": "bf299c5d4060b0a9ec52",
+    "url": "/framer-motion-react/static/js/2.0e4f13db.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/framer-motion-react/static/js/2.0e4f13db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5d92100c80b7c659b4",
+    "url": "/framer-motion-react/static/js/main.0b904d36.chunk.js"
+  },
+  {
+    "revision": "d219708f8e3277ddb488",
+    "url": "/framer-motion-react/static/js/runtime-main.0d2f74b1.js"
+  }
+]);
